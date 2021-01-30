4e2e4fc5c67399816d06073e332374f53b1f7aec
rodrigo <rodrigoespinosa24@gmail.com>
2021-01-30 02:53:18 +0100

    mancho

--- resources/views/user/config.blade.php
+++ resources/views/user/config.blade.php
@@ -6,6 +6,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+       
             <div class="card">
                 <div class="card-header">Configuracion</div>
 
